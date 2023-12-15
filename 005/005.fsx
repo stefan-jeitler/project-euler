e065fcbea248c299efb92670784739b67b512e61
Stefan <stefan.jeitler@outlook.com>
2023-12-15 23:02:49 +0100

    problem 5 solved

---
+++ 005/005.fsx
@@ -0,0 +1,9 @@
+let isEvenlyDvisible range number =
+    range |> List.forall (fun x -> number % x = 0)
+
+let result =
+    Seq.initInfinite id
+    |> Seq.skip 20
+    |> Seq.map ((*) 20)
+    |> Seq.find (isEvenlyDvisible [2 .. 20])
+
