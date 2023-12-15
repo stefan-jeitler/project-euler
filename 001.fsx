8781b649c6017271fdb4d71eafc4ed3c010a2de4
Stefan <stefan.jeitler@outlook.com>
2023-12-15 13:18:36 +0100

    problem 1 solved

---
+++ 001.fsx
@@ -0,0 +1,23 @@
+let numbers = [ 1..999 ]
+
+let multipliers = [ 3; 5 ]
+
+let isMultiple n m =
+    match (n % m) with
+    | 0 -> true
+    | _ -> false
+
+let rec findFirstMultiple (m: int list) n =
+    match m with
+    | [] -> None
+    | head :: tail ->
+        if (isMultiple n head) then
+            Some n
+        else
+            (findFirstMultiple tail n)
+
+let result =
+    numbers 
+    |> List.map (findFirstMultiple multipliers) 
+    |> List.choose id 
+    |> List.sum
