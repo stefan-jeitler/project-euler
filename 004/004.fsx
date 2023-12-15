ea6935b0b92a9fb0eaf16ae0056b01e5c2f1ee4e
Stefan <stefan.jeitler@outlook.com>
2023-12-15 21:29:10 +0100

    problem 4 solved

---
+++ 004/004.fsx
@@ -0,0 +1,15 @@
+let isPalindrom c =
+    let cAsString = string c
+    let cAsStringReversed = cAsString |> Seq.rev |> System.String.Concat
+
+    cAsString = cAsStringReversed
+
+let largestPalindromOf value =
+    let numbers = { 1..value }
+
+    numbers 
+    |> Seq.collect (fun x -> numbers |> Seq.map (fun y -> x * y)) 
+    |> Seq.filter isPalindrom 
+    |> Seq.max
+
+let result = largestPalindromOf 999
