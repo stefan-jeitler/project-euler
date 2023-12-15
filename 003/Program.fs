c4d4234975db383791ab94b62fd504b1881f75fd
Stefan <stefan.jeitler@outlook.com>
2023-12-15 20:51:18 +0100

    reorganize repo structure

---
+++ 003/Program.fs
@@ -0,0 +1,13 @@
+﻿let rec calculatePrimeFactors product factor (acc: uint64 list) =
+    if factor > product then
+        acc
+    elif (product % factor = 0UL) then
+        calculatePrimeFactors (product / factor) (factor + 1UL) (factor :: acc)
+    else
+        calculatePrimeFactors product (factor + 1UL) (acc)
+
+let primeFactors n = calculatePrimeFactors n 2UL []
+
+let result = primeFactors 600851475143UL
+printfn $"largest prime factor is %A{result}"
+
