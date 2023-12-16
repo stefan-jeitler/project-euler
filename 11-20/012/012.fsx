0571348e811b049564faee3302606f915eb6e945
Stefan <stefan.jeitler@outlook.com>
2023-12-16 23:18:07 +0100

    problem 12 solved

--- 11-20/012/012.fsx
+++ 11-20/012/012.fsx
@@ -1,18 +1,18 @@
 let findDivisors (n: uint64) =
     let rec innerFn (d: uint64) acc =
-        let limit = d |> float |> sqrt |> uint64
+        let limit = n |> float |> sqrt |> uint64
 
-        if d > limit then acc
-        elif n % d = 0UL then innerFn (d + 1UL) ([ d; n / d ] @ acc)
-        else innerFn (d + 1UL) acc
+        if d > limit then
+            acc
+        elif n % d = 0UL then
+            let accumulator = if n / d <> d then (n / d) :: acc else acc
+            innerFn (d + 1UL) (d :: accumulator)
+        else
+            innerFn (d + 1UL) acc
 
     innerFn 2UL [ 1UL; n ]
 
-
-findDivisors 10UL
-10UL |> float |> sqrt |> uint64
-
-let triangleNumbers =
+let result =
     Seq.initInfinite (fun x -> (uint64 x) + 1UL)
     |> Seq.map (fun x -> { 1UL .. x } |> Seq.sum)
     |> Seq.map findDivisors
