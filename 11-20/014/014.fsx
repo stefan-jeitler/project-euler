23e49e8aa4237b8d52ba6fe799b2bb0ee1fd63fb
Stefan <stefan.jeitler@outlook.com>
2023-12-17 15:28:31 +0100

    problem 14 solved

---
+++ 11-20/014/014.fsx
@@ -0,0 +1,23 @@
+
+let isEven n = n % 2UL = 0UL
+
+let next n = 
+    if isEven n then n / 2UL
+    else 3UL * n + 1UL
+
+let generate start =
+    let rec innerFn n acc = 
+        if n = 1UL then
+            n :: acc
+        else
+            innerFn (next n) (n :: acc)
+
+    innerFn start []    
+
+let result = 
+    { 1UL .. 1_000_000UL } 
+    |> Seq.map (fun x -> x, (generate x))
+    |> Seq.map (fun (n, s) -> n, s.Length)
+    |> Seq.maxBy snd
+
+let (number, length) = result
