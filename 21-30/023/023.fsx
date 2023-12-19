f84fc103c4dccf57a9a2104bbfd47dbbdf03a64b
Stefan <stefan.jeitler@outlook.com>
2023-12-19 23:48:57 +0100

    problem 23 solved

---
+++ 21-30/023/023.fsx
@@ -0,0 +1,23 @@
+let sumOfDivisors =
+    function
+    | 1UL -> 1UL
+    | n -> { 1UL .. (n / 2UL) } |> Seq.filter (fun x -> n % x = 0UL) |> Seq.sum
+
+let isAbundant n = sumOfDivisors n > n
+
+let totalRange = [ 1UL .. 28123UL ]
+
+let abundantNumbers =
+    totalRange
+    |> List.filter isAbundant
+
+let sumOfAllAbundantNumbers =
+    abundantNumbers
+    |> Seq.collect (fun a -> abundantNumbers |> Seq.map (fun b -> a + b))
+    |> set
+
+let result = 
+    totalRange
+    |> List.filter (fun x -> not (sumOfAllAbundantNumbers.Contains x))
+    |> List.sum
+
