f2703be4753da0693a769ed6270994bd0b7997b8
Stefan <stefan.jeitler@outlook.com>
2023-12-22 13:45:04 +0100

    problem 28 & 29 refactored

--- 21-30/028/028.fsx
+++ 21-30/028/028.fsx
@@ -1,15 +1,15 @@
 let dimensions = [ 1UL..2UL..1001UL]
 
-let sumOfSingleGrid dimension = 
-        let maxOfGrid = pown dimension 2
-        let maxOfPreviousGrid = (max (pown (dimension - 2UL) 2) 1UL)
-        let offset = maxOfPreviousGrid
+let sumOfSingleDimension dimension = 
+        let maxValue = pown dimension 2
+        let maxOfPreviousDimension = (max (pown (dimension - 2UL) 2) 1UL)
+        let offset = maxOfPreviousDimension
         let steps = dimension - 1UL
 
         let bottomRight = offset + steps
         let bottomLeft = bottomRight + steps
         let topLeft = bottomLeft + steps
-        let topRight = maxOfGrid
+        let topRight = maxValue
 
         bottomRight + bottomLeft + topLeft + topRight
 
@@ -17,6 +17,6 @@ let rec sumDiagonals (dimensions: uint64 list) =
     match dimensions with
     | [] -> 0UL
     | head :: tail when head = 1UL -> 1UL + sumDiagonals tail
-    | head :: tail -> (sumOfSingleGrid head) + sumDiagonals tail
+    | head :: tail -> (sumOfSingleDimension head) + sumDiagonals tail
 
 let result = sumDiagonals dimensions
--- 21-30/029/029.fsx
+++ 21-30/029/029.fsx
@@ -2,6 +2,7 @@ let a = [ 2I..100I ]
 let b = [ 2..100 ]
 
 let result = 
-    a |> List.collect (fun a -> b |> List.map (fun b -> pown a b))
+    a 
+    |> List.collect (fun a -> b |> List.map (fun b -> pown a b))
     |> List.distinct
     |> List.length
