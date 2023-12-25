4a9ab95a67a269c0dd31ab98f82c4dc74305b912
Stefan <stefan.jeitler@outlook.com>
2023-12-25 20:36:42 +0100

    problem 33 solved

---
+++ 31-40/033/033.fsx
@@ -0,0 +1,40 @@
+let findFirstSharedDigit n d =
+    let d' = string d
+    let firstDigit = n / 10
+    let secondDigit = n % 10
+
+    match firstDigit, secondDigit with
+    | x, _ when x <> 0 && d'.Contains(string x) -> Some x
+    | _, y when y <> 0 && d'.Contains(string y) -> Some y
+    | _ -> None
+
+let removeDigit n d =
+    let first = n / 10
+    let second = n % 10
+
+    if first = d then second
+    elif second = d then first
+    else n
+
+let fractions =
+    [ for n in [ 10..99 ] do
+          for d in [ 10..99 ] do
+              if n < d then
+                  yield (n, d) ]
+
+let calcCuriousFraction (n, d) =
+    let q = (float n) / (float d)
+    let sharedDigit = findFirstSharedDigit n d
+
+    match sharedDigit with
+    | None -> None
+    | Some x ->
+        let n = removeDigit n x |> float
+        let d = removeDigit d x |> float
+
+        if (n < d) && (n / d) = q then Some q else None
+
+let result =
+    fractions |> List.map calcCuriousFraction |> List.choose id |> List.reduce (*)
+
+// 0.01 => 1/100
