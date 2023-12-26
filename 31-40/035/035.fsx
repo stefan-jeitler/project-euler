f7e567256eb9ac3bf5f4ac7abe9e88ad08d19db3
Stefan <stefan.jeitler@outlook.com>
2023-12-26 16:28:03 +0100

    problem 35 solved

---
+++ 31-40/035/035.fsx
@@ -0,0 +1,32 @@
+let isPrime n =
+    if n < 2 then
+        false
+    else
+        let limit = n |> float |> sqrt |> int
+        { 2..limit } |> Seq.exists (fun x -> n % x = 0) |> not
+
+let allRotations n =
+    let digitsCount = floor ((log10 (float n)) + 1.) |> int
+
+    let rec loop x (acc: int list) =
+        if x = n && acc.Length > 0 then
+            acc
+        else
+            let firstDigit = x / (pown 10 (digitsCount - 1))
+            let remaining = x % (pown 10 (digitsCount - 1))
+
+            let next = (remaining * 10) + firstDigit
+
+            loop next (next :: acc)
+
+    loop n []
+
+let allRotationsArePrimes n =
+    let rotations = allRotations n
+
+    let r = rotations |> List.filter (fun x -> not (isPrime x))
+    r.Length = 0
+
+let primesBelowOneMillion = { 2..1_000_000 } |> Seq.filter isPrime
+
+let result = primesBelowOneMillion |> Seq.filter allRotationsArePrimes |> Seq.length
