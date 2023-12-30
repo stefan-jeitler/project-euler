62c70d717a884a4b7945a1c523e56239661e9a98
Stefan <stefan.jeitler@outlook.com>
2023-12-30 21:03:19 +0100

    problem 40 solved

---
+++ 31-40/040/040.fsx
@@ -0,0 +1,18 @@
+let fraction =
+    let rec loop n =
+        seq {
+            yield! n |> string
+            yield! loop (n + 1)
+        }
+
+    loop 1
+
+let nthElementsOfInterest = [ 1; 10; 100; 1000; 10_000; 100_000; 1_000_000 ] |> set
+
+let result =
+    fraction
+    |> Seq.take 1_000_000
+    |> Seq.indexed
+    |> Seq.filter (fun (i, _) -> nthElementsOfInterest.Contains(i + 1))
+    |> Seq.map (snd >> System.Char.GetNumericValue >> int)
+    |> Seq.reduce (*)
