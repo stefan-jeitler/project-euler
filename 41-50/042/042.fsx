c53968912f34157600daa1d4eebbe81e475c60fd
Stefan <stefan.jeitler@outlook.com>
2023-12-31 22:10:36 +0100

    problem 42 refactored

--- 41-50/042/042.fsx
+++ 41-50/042/042.fsx
@@ -1,5 +1,6 @@
-open System.IO
+let isInteger n = (n % 1.) < System.Double.Epsilon
 
+open System.IO
 let filePath = Path.Combine(__SOURCE_DIRECTORY__, "0042_words.txt")
 
 let words =
@@ -14,21 +15,10 @@ let alphabeticalValueByChar =
 let wordValue c =
     c |> Seq.map (fun x -> alphabeticalValueByChar[x]) |> Seq.sum
 
-let t (n: int) =
-    let n' = float n
-    (n' / 2.) * (n' + 1.)
-
 let isTriangleNumber (n: int) =
+    let x = (sqrt (1. + 8. * float n) - 1.) / 2. 
 
-    let rec loop i =
-        let x = t i
-
-        match x with
-        | x when x < n -> loop (i + 1)
-        | x when x = n -> true
-        | _ -> false
-
-    loop 1
+    isInteger x
 
 let result =
     words |> List.map wordValue |> List.filter isTriangleNumber |> List.length
