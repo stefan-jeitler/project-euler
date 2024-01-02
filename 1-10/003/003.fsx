bc55417605015373fc05c452953c89ed652b1a9f
Stefan <stefan.jeitler@outlook.com>
2024-01-02 15:23:09 +0100

    problem 3 refactored

--- 1-10/003/003.fsx
+++ 1-10/003/003.fsx
@@ -8,4 +8,6 @@ let rec calculatePrimeFactors product factor (acc: uint64 list) =
 
 let primeFactors n = calculatePrimeFactors n 2UL []
 
-let result = primeFactors 600851475143UL
+let result = 
+    primeFactors 600851475143UL
+    |> List.max
