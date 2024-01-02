1d884d078bdda5266ec592835d4be77f4b8b23e5
Stefan <stefan.jeitler@outlook.com>
2024-01-02 12:36:36 +0100

    problem 44 solved

---
+++ 41-50/044/044.fsx
@@ -0,0 +1,23 @@
+let isPentagonal n =
+    // quadratic equation
+    let x1 = (1. + sqrt (1. + 24. * float n)) / 6.
+
+    x1 % 1. = 0
+
+let pentagonal n = n * (3 * n - 1) / 2
+
+let pentagonalPairs =
+    seq {
+        for j in (Seq.initInfinite (fun x -> x + 2)) do
+            for k in ({ j - 1 .. -1 .. 1 } |> Seq.rev) do
+                yield (pentagonal j, pentagonal k)
+    }
+
+let isSumPentagonal (pj, pk) = isPentagonal (pj + pk)
+let isDiffPentagonal (pj, pk) = isPentagonal (pj - pk)
+
+let result =
+    pentagonalPairs
+    |> Seq.skipWhile (fun x -> not ((isSumPentagonal x) && (isDiffPentagonal x)))
+    |> Seq.head
+    |> (fun (pj, pk) -> (pj - pk))
