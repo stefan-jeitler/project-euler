61b8cb874ec2755172211dc69d2d8340e7d57de9
Stefan <stefan.jeitler@outlook.com>
2024-01-03 14:35:10 +0100

    problem 50 fixed

--- 41-50/050/050.fsx
+++ 41-50/050/050.fsx
@@ -1,5 +1,3 @@
-// solution inspired by: https://euler.stephan-brumme.com/50/
-
 let isPrime n =
     if n < 2 then
         false
@@ -9,28 +7,31 @@ let isPrime n =
 
 let primes = Seq.initInfinite (fun x -> x + 2) |> Seq.filter isPrime
 
+let limit = 1_000_000
+
 let primeSums =
-    primes
+    Seq.initInfinite id
     |> Seq.indexed
-    |> Seq.map (fun (i, x) -> primes |> Seq.take i |> Seq.sum)
-    |> Seq.takeWhile (fun x -> x < 1_000_000)
+    |> Seq.map (fun (i, _) -> primes |> Seq.take i |> Seq.sum)
+    |> Seq.takeWhile (fun x -> x < limit)
     |> Seq.toList
 
-let (maxPrimeIdx, maxPrimeSum) =
+let consecutivePrimeSummands sum idx =
+    let rec loop fi s i =
+        if fi >= i then
+            s, 0
+        elif (isPrime (primeSums[i])) then
+            primeSums[i], i
+        elif (isPrime (primeSums[i] - primeSums[fi])) then
+            primeSums[i] - primeSums[fi], i - fi
+        else
+            loop (fi + 1) primeSums[i] i
+
+    loop 0 sum idx
+
+let result =
     primeSums
     |> Seq.indexed
+    |> Seq.map (fun (i, s) -> consecutivePrimeSummands s i)
     |> Seq.maxBy snd
-
-let findMaxPrimeSum max = 
-    let rec loop idx =
-        let m = primeSums[maxPrimeIdx] - primeSums[idx]
-        if idx >= maxPrimeIdx then
-            None
-        elif isPrime m then
-            Some m
-        else
-            loop (idx + 1)
-
-    loop 1
-
-let result = findMaxPrimeSum maxPrimeSum
+    |> fst
