6032d95552ba8be842f46c41eb9ac17085a9a074
Stefan <stefan.jeitler@outlook.com>
2024-01-06 14:43:48 +0100

    problem 55 solved

---
+++ 51-60/055/055.fsx
@@ -0,0 +1,30 @@
+let isPalindrom (n: bigint) =
+    let n' = string n
+    let nReversed = n' |> Seq.rev |> System.String.Concat
+
+    n' = nReversed
+
+let reverse n =
+    n
+    |> string
+    |> Seq.rev
+    |> System.String.Concat
+    |> System.Numerics.BigInteger.Parse
+
+let isLychrelNumberInLessThen50Iterations n =
+    let rec loop n i =
+        let nReversed = reverse n
+        let sum = n + nReversed
+
+        if i = 50 then true
+        elif isPalindrom sum then false
+        else loop sum (i + 1)
+
+    loop n 0
+
+isLychrelNumberInLessThen50Iterations 4992I
+
+let result =
+    { 1I .. 9_999I }
+    |> Seq.filter isLychrelNumberInLessThen50Iterations
+    |> Seq.length
