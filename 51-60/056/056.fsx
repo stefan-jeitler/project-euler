699577b0f88a619dd62ac8a5f5c86944c515b663
Stefan <stefan.jeitler@outlook.com>
2024-01-06 14:53:08 +0100

    problem 56 solved

---
+++ 51-60/056/056.fsx
@@ -0,0 +1,13 @@
+let pairs =
+    [ for a in [ 1I..100I ] do
+          for b in [ 1..99 ] do
+              a, b ]
+
+let sumOfDigits n =
+    n |> string |> Seq.map (string >> System.Numerics.BigInteger.Parse) |> Seq.sum
+
+let result =
+    pairs
+    |> Seq.map (fun (a, b) -> pown a b)
+    |> Seq.map sumOfDigits
+    |> Seq.max
