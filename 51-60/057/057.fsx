057063be50f940957dc9bb22264bbb7ac60b1add
Stefan <stefan.jeitler@outlook.com>
2024-01-09 12:59:11 +0100

    problem 57 refactored

--- 51-60/057/057.fsx
+++ 51-60/057/057.fsx
@@ -1,18 +1,16 @@
-let expansion length =
-    let rec loop i n d =
-        if i = length then
-            n, d
-        else
-            loop (i + 1) (2I * d + n) (d + n)
+let expansions  =
+    let rec loop n d =
+        seq {
+            yield (n, d)
+            yield! loop (2I * d + n) (d + n)
+        }
 
-    loop 0 1I 1I
+    loop 1I 1I
 
 let digits n = n |> string |> _.Length
 
-let numeratorExceedsDenominatorDigits (n, d) = digits n > digits d
-
 let result =
-    { 1..1000 }
-    |> Seq.map expansion
-    |> Seq.filter numeratorExceedsDenominatorDigits
+    expansions
+    |> Seq.take 1000
+    |> Seq.filter (fun (n, d) -> digits n > digits d)
     |> Seq.length
